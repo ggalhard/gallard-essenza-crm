9406adb7bc5ed7cbe45acee8aa30a3f47ef9df0e
ggalhard <agencyaureon@gmail.com>
2026-09-02 11:09:17 -0300

    feat: suportar pedidos, compras e parcelas por lista

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Subir esse número a cada mudança no app, senão o celular que já tem o
 // Caderno instalado continua servindo a versão antiga do cache. O activate
 // abaixo apaga todo cache com nome diferente deste.
-const CACHE = "caderno-gallard-v4";
+const CACHE = "caderno-gallard-v5";
 const ASSETS = [
   "./",
   "./index.html",
